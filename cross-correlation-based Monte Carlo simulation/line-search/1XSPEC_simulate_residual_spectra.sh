c71fa58bd290b0d9ea37b2b0b5fc87c2ba078595
Yerong Xu <53796022+yrxu@users.noreply.github.com>
2023-12-30 01:09:53 +0100

    Update 1XSPEC_simulate_residual_spectra.sh

--- cross-correlation-based Monte Carlo simulation/line-search/1XSPEC_simulate_residual_spectra.sh
+++ cross-correlation-based Monte Carlo simulation/line-search/1XSPEC_simulate_residual_spectra.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-export OMP_NUM_THREADS=4
-mkdir ${PWD}/simulation
-DIR_home=${PWD}/simulation
-mkdir ${DIR_home}/MC_spectrum
-mkdir ${DIR_home}/res
-MC_spectrum=${DIR_home}/MC_spectrum
-res_spectrum=${DIR_home}/res
+
