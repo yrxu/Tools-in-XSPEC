fc2785b137648ba84e28aec797ab31ded1be61dc
Yerong Xu <53796022+yrxu@users.noreply.github.com>
2023-12-31 16:31:50 +0100

    Update 2XSPEC_produce_model.sh

--- cross-correlation-based Monte Carlo simulation/line-search/2XSPEC_produce_model.sh
+++ cross-correlation-based Monte Carlo simulation/line-search/2XSPEC_produce_model.sh
@@ -20,52 +20,52 @@ do
 echo "linewidth: ${linewidth[$a]} and number of points: ${num_points}"
 routine_sim=${DIR_home}/simulated_lw${linewidth[$a]}_model_${num_points}.xcm
 
-logmin=$(echo "l(${Emin})/l(10)" | bc -l)
+logmin=$(echo "l(${Emin})/l(10)" | bc -l)  ##scanned in a logarithmic scale
 logmax=$(echo "l(${Emax})/l(10)" | bc -l)
 logestep=$(echo "(l(${Emax})/l(10)-l(${Emin})/l(10))/(${num_points}-1)" | bc -l)
 echo ${logestep} ${logmin} ${logmax}
 echo "start to make the routine file to generate model"
-echo "@${xspec_startup_xcm}" > ${routine_sim}
-echo "query yes"            >> ${routine_sim}
-echo "abun wilm"            >> ${routine_sim}
-echo "data 2:2 none"        >> ${routine_sim}
-echo "model gaus"           >> ${routine_sim}
-echo "/*"                   >> ${routine_sim}
+echo "@${xspec_startup_xcm}"                                        > ${routine_sim}
+echo "query yes"                                                   >> ${routine_sim}
+echo "abun wilm"                                                   >> ${routine_sim}
+echo "data 2:2 none"                                               >> ${routine_sim}
+echo "model gaus"                                                  >> ${routine_sim} ### if you want to obtain the rest-frame results, use zgaussian and set the redshift
+echo "/*"                                                          >> ${routine_sim}
 
 
 
 echo "start to generate loop" 
 for y in $(seq 0 1 $((${num_points}-1)))
 do 
-	energy=$(echo "scale=8; e((${logmin}+$y*$logestep)*l(10))"| bc -l )
+	energy=$(echo "scale=8; e((${Emin}+$y*$logestep)*l(10))"| bc -l )
 	lw=$(echo "scale=8; ${energy}*${linewidth[$a]}/300000"| bc -l )
 	echo "Energy: ${energy} keV; linewidth: ${lw} keV"
-	echo "#Energy: ${energy} keV; linewidth: ${lw} keV" >> ${routine_sim}
+	echo "#Energy: ${energy} keV; linewidth: ${lw} keV"        >> ${routine_sim}
 	echo "index" $(($y+1))"/"${num_points}
 
-	echo "new 1 ${energy} -1"         >> ${routine_sim}
-	echo "new 2 ${lw} -1"             >> ${routine_sim}
-	echo "new 3 1 -1"                 >> ${routine_sim}
-	echo " "                          >> ${routine_sim}
-	echo "ignore 1:**-0.4 1.77-**"    >> ${routine_sim}
-	echo "cpd /xs"                    >> ${routine_sim}
-	echo "setp e"                     >> ${routine_sim}
-	echo "plot uf"                   >> ${routine_sim}
-	echo "setplot command wd ${y}_model_rgs"         >> ${routine_sim}
-	echo "plot "                      >> ${routine_sim}
-	echo "setplot list"               >> ${routine_sim}
-	echo "mv ${y}_model_rgs.qdp ${model_dir} "                       >> ${routine_sim}
-	echo "setplot delete all"         >> ${routine_sim}	
-	echo " "                          >> ${routine_sim}
+	echo "new 1 ${energy} -1"                                  >> ${routine_sim}
+	echo "new 2 ${lw} -1"                                      >> ${routine_sim}
+	echo "new 3 1 -1"                                          >> ${routine_sim}
+	echo " "                                                   >> ${routine_sim}
+	echo "ignore 1:**-${Emin} ${Emax}-**"                      >> ${routine_sim}
+	echo "cpd /null"                                           >> ${routine_sim}
+	echo "setp e"                                              >> ${routine_sim}
+	echo "plot uf"                                             >> ${routine_sim}
+	echo "setplot command wd ${y}_model_rgs"                   >> ${routine_sim}
+	echo "plot "                                               >> ${routine_sim}
+	echo "setplot list"                                        >> ${routine_sim}
+	echo "mv ${y}_model_rgs.qdp ${model_dir} "                 >> ${routine_sim}
+	echo "setplot delete all"                                  >> ${routine_sim}	
+	echo " "                                                   >> ${routine_sim}
 done
 
-echo "exit"                 >> ${routine_sim}
+echo "exit"                                                        >> ${routine_sim}
 
 xspec<<EOF
 @${routine_sim}
 EOF
 
-echo "merge residual spectra into one file"
+echo "merge model spectra into one file"
 
 python3<<EOF
 import pandas as pd
