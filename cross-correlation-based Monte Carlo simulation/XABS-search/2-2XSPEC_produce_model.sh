3bfab019a11f3712e572cd996ee6e94a0d5ca486
Yerong Xu <53796022+yrxu@users.noreply.github.com>
2024-01-04 00:44:29 +0100

    Update 2-2XSPEC_produce_model.sh

--- cross-correlation-based Monte Carlo simulation/XABS-search/2-2XSPEC_produce_model.sh
+++ cross-correlation-based Monte Carlo simulation/XABS-search/2-2XSPEC_produce_model.sh
@@ -15,66 +15,24 @@ xi_min=0.0
 xi_max=5.0      ### scanned logxi grids
 xi_step=0.1
 zv_min=0
-zv_max=105000   ### scanned velocity grids
-linewidth=(500 1500 4500 10000)  ### scanned linewidth grids
-vstep_list=(300 700 1500)        ### corresponding step of velocities
-
-#logNH_min=19.0 ### cm^-2
-#logNH_max=23.0
-#logNH_step=1.0
-
-#min_energy=0.4
-#max_energy=1.77
+zv_max=105000   ###km/s scanned velocity grids
+linewidth=(500 1500 4500 10000)       ### scanned linewidth grids
+vstep_list=(300 700 1500 3000)        ### corresponding step of velocities
 
 xspec_startup_xcm=${PWD}/nthcomp+relxillCp.xcm  #change the location of data into a global location not e.g. ../../analysis
-
-################save real residual spectrum
-#linewidth=(0 100 500 1000 1500 2000 5000)
-#num_points=2000
-
-#Generate a powerlaw model first
-#xspec<<EOF
-#@${xspec_startup_xcm}
-#query yes
-#abun lpgs
-#model pow
-#/*
-#new 1 2
-#cpd /null
-#setp e
-#pl eeuf
-#setp command wd model_power
-#plot
-#setp del all
-#exit
-#EOF
-#mv model_power.qdp ${model_dir}
-
-for a in 0 
+model_plus_plasma="zashift*mtable{xabs_xs.fits}*pow"
+################create routines to produce model spectra with absorption lines
+for a in 0 1 2 3
 do
 echo "linewidth: ${linewidth[$a]} km/s with a step of velocity: ${vstep_list[$a]} km/s"
-	#for lognh in $(seq ${logNH_min} ${logNH_step} ${logNH_max})
-	#do
-	#	NH=$(echo "scale=8; e((${lognh}-24.0)*l(10))"| bc -l )
-	#	echo "logNH: ${lognh} cm^-2 with a step of Delta logNH: ${logNH_step}"
-	#	routine_sim=${DIR_home}/simulated_lw${linewidth[$a]}_NH${lognh}_model.xcm
-	
+routine_sim=${DIR_home}/simulated_lw${linewidth[$a]}_model.xcm
 
-	routine_sim=${DIR_home}/simulated_lw${linewidth[$a]}_model.xcm
-
-
-#logmin=$(echo "l(${min_energy})/l(10)" | bc -l)
-#logmax=$(echo "l(${max_energy})/l(10)" | bc -l)
-#logestep=$(echo "(l(${max_energy})/l(10)-l(${min_energy})/l(10))/(${num_points}-1)" | bc -l)
-#echo ${logestep} ${logmin} ${logmax}
-	echo "logxi = " ${xi_min} "-" ${xi_max} ", with step " ${xi_step}
-#echo "zv = " ${zv_min} "-" ${zv_max} ", with step " ${vstep_step}
-	echo "start to make the routine file to generate model"
-	echo "@${xspec_startup_xcm}" > ${routine_sim}
-	echo "query yes"            >> ${routine_sim}
-	echo "abun lpgs"            >> ${routine_sim}
-#echo "data 2:2 none"        >> ${routine_sim}
-	echo "model zashift*mtable{xabs_xs.fits}*pow"     >> ${routine_sim}
+echo "logxi = " ${xi_min} "-" ${xi_max} ", with step " ${xi_step}	
+echo "start to make the routine file to generate model"
+echo "@${xspec_startup_xcm}" > ${routine_sim}
+echo "query yes"            >> ${routine_sim}
+echo "abun lpgs"            >> ${routine_sim}
+echo "model zashift*mtable{xabs_xs.fits}*pow"     >> ${routine_sim}
 	echo "/*"                   >> ${routine_sim}
 	echo "new 1 ${redshift} "                   >> ${routine_sim}
 	echo "new 5 1 "                   >> ${routine_sim}
